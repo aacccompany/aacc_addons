9d9a7f0362593d5e6aa585fe46906aa58eea5ff7
aacccompany <aaccth.office@gmail.com>
2024-09-18 11:23:35 +0700

    Update __manifest__.py

--- pos_solution-main/__manifest__.py
+++ pos_solution-main/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "pos_solution",
+    'name': "POS Payment QR Code",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
