6b1191ac071ac6ec6190ffb51246530e6ffd8d14
AA&CC <aaccth.office@gmail.com>
2026-01-21 13:48:25 +0700

    test

--- om_dbd_search/__manifest__.py
+++ om_dbd_search/__manifest__.py
@@ -4,7 +4,7 @@
     'license': 'OPL-1',
     'category': 'Contacts/Accounting/Localization/Master Data',
     'summary': 'Retrieve company information from DBD Open Data API with 13-digit tax ID.',
-    'price': 18,
+    'price': 20,
     'currency': 'USD',
     'support': 'aaccth.office@gmail.com',
     'depends': ['base', 'contacts'],
@@ -16,5 +16,4 @@
     'installable': True,
     'application': True,
     'images': ['static/description/banner.png'],
-
 } 
