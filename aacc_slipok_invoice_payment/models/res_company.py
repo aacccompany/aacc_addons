070cc01843a5bf55e13947b9036ba2e9d7381eff
aacccompany <aaccth.office@gmail.com>
2026-03-12 15:27:10 +0700

    commit file

---
+++ aacc_slipok_invoice_payment/models/res_company.py
@@ -0,0 +1,13 @@
+from odoo import fields, models
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    aacc_line_qr_code = fields.Binary(string="LINE Payment QR Code", copy=False)
+    
+    slip_verification_provider = fields.Selection(
+        [('slipok', 'SlipOK'), ('slip2go', 'Slip2Go')],
+        string="Slip Verification Provider",
+        default='slipok'
+    )
+    slip2go_api_secret = fields.Char(string="Slip2Go API Secret")
